255163c8d4d74e7a4072bf8236a11fc0a367344c
Iyed <iyedabdelli73@gmail.com>
2024-03-27 17:21:12 +0100

    Update EventSection & EvenCarousel to render events from array

--- index.php
+++ index.php
@@ -38,24 +38,91 @@
 
 
         <?php include 'src/Views/home/hero.php' ?>
-        
 
-        <?php //include 'src/Views/home/anotherFeatureSection.php' ?>
-        
+        <?php
+        // some dummy data
+        $categories = ["Category 1", "Category 2", "Category 3"];
+        $events = [
+            [
+                "name" => "Event 1",
+                "category" => "Category 1",
+                "description" => "Description of Event 1",
+                "price" => 10.99,
+                "startSellTime" => "2024-03-30 09:00:00",
+                "endSellTime" => "2024-03-30 18:00:00",
+                "eventDate" => "2024-04-01",
+                "image" => "image1.jpg"
+            ],
+            [
+                "name" => "Event 2",
+                "category" => "Category 2",
+                "description" => "Description of Event 2",
+                "price" => 15.99,
+                "startSellTime" => "2024-03-31 09:00:00",
+                "endSellTime" => "2024-03-31 18:00:00",
+                "eventDate" => "2024-04-02",
+                "image" => "image2.jpg"
+            ],
+            [
+                "name" => "Event 3",
+                "category" => "Category 3",
+                "description" => "Description of Event 3",
+                "price" => 20.99,
+                "startSellTime" => "2024-04-01 09:00:00",
+                "endSellTime" => "2024-04-01 18:00:00",
+                "eventDate" => "2024-04-03",
+                "image" => "image3.jpg"
+            ],
+            [
+                "name" => "Event 4",
+                "category" => "Category 1",
+                "description" => "Description of Event 4",
+                "price" => 25.99,
+                "startSellTime" => "2024-03-25 10:00:00",
+                "endSellTime" => "2024-03-30 18:00:00",
+                "eventDate" => "2024-03-27",
+                "image" => "image4.jpg"
+            ],
+            [
+                "name" => "Event 5",
+                "category" => "Category 3",
+                "description" => "Description of Event 5",
+                "price" => 30.99,
+                "startSellTime" => "2024-03-20 09:00:00",
+                "endSellTime" => "2024-03-30 18:00:00",
+                "eventDate" => "2024-03-22",
+                "image" => "image5.jpg"
+            ],
+            [
+                "name" => "Event 6",
+                "category" => "Category 3",
+                "description" => "Description of Event 5",
+                "price" => 30.99,
+                "startSellTime" => "2024-03-20 09:00:00",
+                "endSellTime" => "2024-03-30 18:00:00",
+                "eventDate" => "2024-03-22",
+                "image" => "image5.jpg"
+            ]
+        ];
+        ?>
 
-        <?php include 'src/Views/home/eventSection.php' ?>
+
+        <?php
+
+        include 'src/Views/home/eventSection.php';
+        ?>
         
 
         <?php //include 'src/Views/home/featuresSection.php' ?>
-        
 
-        <?php include 'src/Views/home/eventCarousel.php' ?>
-        
-        <?php include 'src/Views/home/eventCarousel.php' ?>
-        
-        <?php include 'src/Views/home/eventCarousel.php' ?>
-        
-        <?php include 'src/Views/home/eventCarousel.php' ?>
+
+        <?php
+
+        foreach ($categories as $category) {
+            include 'src/Views/home/eventCarousel.php';
+        }
+        ?>
+
 
 
         <?php //include 'src/Views/home/bannerSection.php' ?>
@@ -65,6 +132,9 @@
         
 
         <?php //include 'src/Views/home/factsSection.php' ?>
+
+
+        <?php include 'src/Views/home/anotherFeatureSection.php' ?>
 
 
         <?php include 'src/Views/home/testimonialSection.php' ?>
--- src/Views/home/eventCarousel.php
+++ src/Views/home/eventCarousel.php
@@ -1,119 +1,36 @@
 <div class="container-fluid vesitable py-5">
-            <div class="container py-5">
-                <h1 class="mb-0">[EVENT_TYPE]</h1>
-                <div class="owl-carousel vegetable-carousel justify-content-center">
-                    <div class="border border-primary rounded position-relative vesitable-item">
-                        <div class="vesitable-img">
-                            <img src="img/vegetable-item-6.jpg" class="img-fluid w-100 rounded-top" alt="">
+    <div class="container py-5">
+        <h1 class="mb-0"><?php echo $category; ?></h1>
+        <div class="owl-carousel vegetable-carousel justify-content-center">
+            <?php
+            $hasEvents = false;
+            foreach ($events as $event): ?>
+                <?php
+                // Check if the event category matches the current category and the event is within the selling time
+                if ($event['category'] == $category && strtotime($event['startSellTime']) <= time() && time() <= strtotime($event['endSellTime'])):
+                    $hasEvents = true;
+                    ?>
+                    <div class="border border-primary rounded position-relative event-item">
+                        <div class="event-img">
+                            <img src="<?= $event['image'] ?>" class="img-fluid w-100 rounded-top" alt="">
                         </div>
-                        <div class="text-white bg-primary px-3 py-1 rounded position-absolute" style="top: 10px; right: 10px;">Vegetable</div>
+                        <div class="text-white bg-primary px-3 py-1 rounded position-absolute" style="top: 10px; right: 10px;"><?php echo $event['category']; ?></div>
                         <div class="p-4 rounded-bottom">
-                            <h4>Parsely</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
+                            <h4><?php echo $event['name']; ?></h4>
+                            <p><?php echo $event['description']; ?></p>
                             <div class="d-flex justify-content-between flex-lg-wrap">
-                                <p class="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
-                                <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
+                                <p class="text-dark fs-5 fw-bold mb-0">$<?php echo $event['price']; ?> </p>
+                                <a href="<?php // url_for('eventPage', ['id' => $event['id']]) ?>" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-eye me-2 text-primary"></i> View Event</a>
                             </div>
                         </div>
                     </div>
-                    <div class="border border-primary rounded position-relative vesitable-item">
-                        <div class="vesitable-img">
-                            <img src="img/vegetable-item-1.jpg" class="img-fluid w-100 rounded-top" alt="">
-                        </div>
-                        <div class="text-white bg-primary px-3 py-1 rounded position-absolute" style="top: 10px; right: 10px;">Vegetable</div>
-                        <div class="p-4 rounded-bottom">
-                            <h4>Parsely</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
-                            <div class="d-flex justify-content-between flex-lg-wrap">
-                                <p class="text-dark fs-5 fw-bold mb-0">$4.99 / kg</p>
-                                <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="border border-primary rounded position-relative vesitable-item">
-                        <div class="vesitable-img">
-                            <img src="img/vegetable-item-3.png" class="img-fluid w-100 rounded-top bg-light" alt="">
-                        </div>
-                        <div class="text-white bg-primary px-3 py-1 rounded position-absolute" style="top: 10px; right: 10px;">Vegetable</div>
-                        <div class="p-4 rounded-bottom">
-                            <h4>Banana</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
-                            <div class="d-flex justify-content-between flex-lg-wrap">
-                                <p class="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
-                                <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="border border-primary rounded position-relative vesitable-item">
-                        <div class="vesitable-img">
-                            <img src="img/vegetable-item-4.jpg" class="img-fluid w-100 rounded-top" alt="">
-                        </div>
-                        <div class="text-white bg-primary px-3 py-1 rounded position-absolute" style="top: 10px; right: 10px;">Vegetable</div>
-                        <div class="p-4 rounded-bottom">
-                            <h4>Bell Papper</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
-                            <div class="d-flex justify-content-between flex-lg-wrap">
-                                <p class="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
-                                <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="border border-primary rounded position-relative vesitable-item">
-                        <div class="vesitable-img">
-                            <img src="img/vegetable-item-5.jpg" class="img-fluid w-100 rounded-top" alt="">
-                        </div>
-                        <div class="text-white bg-primary px-3 py-1 rounded position-absolute" style="top: 10px; right: 10px;">Vegetable</div>
-                        <div class="p-4 rounded-bottom">
-                            <h4>Potatoes</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
-                            <div class="d-flex justify-content-between flex-lg-wrap">
-                                <p class="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
-                                <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="border border-primary rounded position-relative vesitable-item">
-                        <div class="vesitable-img">
-                            <img src="img/vegetable-item-6.jpg" class="img-fluid w-100 rounded-top" alt="">
-                        </div>
-                        <div class="text-white bg-primary px-3 py-1 rounded position-absolute" style="top: 10px; right: 10px;">Vegetable</div>
-                        <div class="p-4 rounded-bottom">
-                            <h4>Parsely</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
-                            <div class="d-flex justify-content-between flex-lg-wrap">
-                                <p class="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
-                                <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="border border-primary rounded position-relative vesitable-item">
-                        <div class="vesitable-img">
-                            <img src="img/vegetable-item-5.jpg" class="img-fluid w-100 rounded-top" alt="">
-                        </div>
-                        <div class="text-white bg-primary px-3 py-1 rounded position-absolute" style="top: 10px; right: 10px;">Vegetable</div>
-                        <div class="p-4 rounded-bottom">
-                            <h4>Potatoes</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
-                            <div class="d-flex justify-content-between flex-lg-wrap">
-                                <p class="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
-                                <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="border border-primary rounded position-relative vesitable-item">
-                        <div class="vesitable-img">
-                            <img src="img/vegetable-item-6.jpg" class="img-fluid w-100 rounded-top" alt="">
-                        </div>
-                        <div class="text-white bg-primary px-3 py-1 rounded position-absolute" style="top: 10px; right: 10px;">Vegetable</div>
-                        <div class="p-4 rounded-bottom">
-                            <h4>Parsely</h4>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
-                            <div class="d-flex justify-content-between flex-lg-wrap">
-                                <p class="text-dark fs-5 fw-bold mb-0">$7.99 / kg</p>
-                                <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                <?php endif; ?>
+            <?php endforeach; ?>
+
         </div>
+        <?php if (!$hasEvents): ?>
+            <div class="mt-4 text-center">No events in this category for now.</div>
+
+        <?php endif; ?>
+    </div>
+</div>
