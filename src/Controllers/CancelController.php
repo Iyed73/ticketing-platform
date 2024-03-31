2f4e7885fa614d54e20c38a8adc4e3d23b30e8af
Iyed <iyedabdelli73@gmail.com>
2024-03-31 15:58:50 +0100

    Update Event Reservation controller & create Cancel controller to cancel event reservations

---
+++ src/Controllers/CancelController.php
@@ -0,0 +1,40 @@
+<?php
+require_once "../Models/EventReservationModel.php";
+
+class CancelController {
+    private EventReservationModel $eventReservationModel;
+
+    public function __construct() {
+        $this->eventReservationModel = new EventReservationModel();
+    }
+
+    public function handlePostRequest($userId, $reservationId) {
+        if (!$this->eventReservationModel->isValidReservation($userId, $reservationId)) {
+            http_response_code(401);
+        }
+
+        $this->eventReservationModel->cancelReservation($reservationId);
+
+        header("Location: /home.php");
+    }
+}
+
+
+session_start();
+
+if (!isset($_SESSION["user_id"])) {
+    http_response_code(401);
+    exit();
+}
+
+$userId = $_SESSION["user_id"];
+
+$cancelController = new CancelController();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $reservationId = $_POST["reservation_id"] ?? null;
+    if ($reservationId === null) {
+        http_response_code(400);
+    }
+    $cancelController->handlePostRequest($userId, $reservationId);
+}
