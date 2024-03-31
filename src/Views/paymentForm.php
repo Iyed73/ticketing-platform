a21cf626c6a6bbd0cf3e468300ac2a7f1c0e9d84
Iyed <iyedabdelli73@gmail.com>
2024-03-31 15:38:30 +0100

    Create payment view

---
+++ src/Views/paymentForm.php
@@ -0,0 +1,53 @@
+<div class="container py-5">
+    <div style="margin-top: 20vh;"></div>
+    <form method="post" action="../Controllers/CancelController.php" class="text-center mb-5">
+        <button type="submit" name="cancel" class="btn btn-secondary mb-3 text-white">Cancel</button>
+    </form>
+    <h2>Enter Client Information</h2>
+    <form method="post" action="../Controllers/PaymentController.php">
+        <?php
+        $quantity = $_GET["quantity"] ?? 1;
+        $reservation_id = $_GET["reservation_id"] ?? '';
+        ?>
+        <input type="hidden" name="reservation_id" value="<?php echo $reservation_id; ?>">
+        <?php
+        for ($i = 1; $i <= $quantity; $i++) {
+            ?>
+            <div style="margin-bottom: 12vh;">
+                <h4>Client <?php echo $i; ?></h4>
+                <div class="row mb-3">
+                    <div class="col-sm-4">
+                        <input type="text" class="form-control" name="first_names[]" placeholder="First Name" required>
+                    </div>
+                    <div class="col-sm-4">
+                        <input type="text" class="form-control" name="last_names[]" placeholder="Last Name" required>
+                    </div>
+                    <div class="col-sm-4">
+                        <input type="email" class="form-control" name="emails[]" placeholder="Email" required>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+
+        <hr>
+
+        <h2>Enter Credit Card Information</h2>
+        <div class="row mb-3">
+            <div class="col-sm-6">
+                <label for="credit_card">Credit Card Number</label>
+                <input type="text" class="form-control" name="credit_card" id="credit_card" placeholder="Enter Credit Card Number" required>
+            </div>
+            <div class="col-sm-3">
+                <label for="expiration_date">Expiration Date</label>
+                <input type="text" class="form-control" name="expiration_date" id="expiration_date" placeholder="MM/YYYY" required>
+            </div>
+            <div class="col-sm-3">
+                <label for="cvv">CVV</label>
+                <input type="text" class="form-control" name="cvv" id="cvv" placeholder="CVV" required>
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary text-white">Submit Payment</button>
+    </form>
+</div>
+
---
+++ src/Views/paymentView.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include 'header.php' ?>
+        <link href="../../lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+        <link href="../../lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+
+        <link href="../../Static/CSS/bootstrap.min.css" rel="stylesheet">
+
+        <link href="../../Static/CSS/styles.css" rel="stylesheet">    </head>
+    </head>
+
+    <body>
+
+
+        <?php include 'navbar.php' ?>
+
+        <?php include 'modalSearch.php' ?>
+
+        <?php include 'paymentForm.php' ?>
+
+        <?php include 'footer.php' ?>
+
+
+        <?php include 'copyright.php' ?>
+
+
+        <?php include 'scripts.php'?>
+    </body>
+
+</html>
