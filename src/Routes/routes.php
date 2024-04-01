3d21e0d0c0753d4d2e5d60de1bce989924f0aa18
RayenKasmi <rayenekasmi0@gmail.com>
2024-04-01 07:07:01 +0100

    Readjusted routes

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -1,8 +1,9 @@
 <?php
-$prefix = '/ticketing'; // change the prefix to your project directory if on xampp or to "" if running on php server
+$prefix = '/ticketing-platform'; // change the prefix to your project directory if on xampp or to "" if running on php server
 
 $routes = [
     "{$prefix}/home" => 'src/Controllers/HomeController.php',
+    "{$prefix}/Home" => 'src/Views/home.php',
     "{$prefix}/login" => 'src/Controller/LoginController.php',
     "{$prefix}/register" => 'src/Controller/RegisterController.php',
     "{$prefix}/hello" => 'src/Views/hello.html',
