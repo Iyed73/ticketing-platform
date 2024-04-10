c35bccf23ada666350076aa5b328fdeedbf42473
RayenKasmi <rayenekasmi0@gmail.com>
2024-04-10 02:50:12 +0100

    In commemoration of the founders.

--- src/Views/home/testimonialSection.php
+++ src/Views/home/testimonialSection.php
@@ -1,27 +1,21 @@
 <div class="container-fluid testimonial py-5">
     <div class="container py-5">
         <div class="testimonial-header text-center">
-            <h4 class="text-primary">Our Testimonial</h4>
-            <h1 class="display-5 mb-5 text-dark">Our Clients Saying!</h1>
+            <h4 class="text-primary">The Founders</h4>
+            <h1 class="display-5 mb-5 text-dark">The developers of this website!</h1>
         </div>
         <div class="owl-carousel testimonial-carousel">
             <div class="testimonial-item img-border-radius bg-light rounded p-4">
                 <div class="position-relative">
                     <i class="fa fa-quote-right fa-2x text-secondary position-absolute"
                         style="bottom: 30px; right: 0;"></i>
-                    <div class="mb-4 pb-4 border-bottom border-secondary">
-                        <p class="mb-0">
-                            The fair pricing and excellent customer service exceeded my expectations. I’ve found my
-                            go-to platform for all events!
-                        </p>
-                    </div>
                     <div class="d-flex align-items-center flex-nowrap">
                         <div class="bg-secondary rounded">
-                            <img src="Static/Images/testimonial-2.jpg" class="img-fluid rounded"
+                            <img src="Static/Images/iyed.jpg" class="img-fluid rounded"
                                 style="width: 100px; height: 100px;" alt="">
                         </div>
                         <div class="ms-4 d-block">
-                            <h4 class="text-dark">John</h4>
+                            <h4 class="text-dark">Iyed Abdelli</h4>
                         </div>
                     </div>
                 </div>
@@ -30,19 +24,13 @@
                 <div class="position-relative">
                     <i class="fa fa-quote-right fa-2x text-secondary position-absolute"
                         style="bottom: 30px; right: 0;"></i>
-                    <div class="mb-4 pb-4 border-bottom border-secondary">
-                        <p class="mb-0">
-                            The dedicated support team made my experience unforgettable. Quick responses and helpful
-                            guidance made me feel valued as a customer.
-                        </p>
-                    </div>
                     <div class="d-flex align-items-center flex-nowrap">
                         <div class="bg-secondary rounded">
-                            <img src="Static/Images/testimonial-3.jpg" class="img-fluid rounded"
+                            <img src="Static/Images/rayen.jpg" class="img-fluid rounded"
                                 style="width: 100px; height: 100px;" alt="">
                         </div>
                         <div class="ms-4 d-block">
-                            <h4 class="text-dark">Christina</h4>
+                            <h4 class="text-dark">Rayen Kasmi</h4>
                         </div>
                     </div>
                 </div>
@@ -51,19 +39,43 @@
                 <div class="position-relative">
                     <i class="fa fa-quote-right fa-2x text-secondary position-absolute"
                         style="bottom: 30px; right: 0;"></i>
-                    <div class="mb-4 pb-4 border-bottom border-secondary">
-                        <p class="mb-0">
-                            Booking tickets was a breeze! The user-friendly interface and diverse event options made the
-                            process enjoyable and hassle-free.
-                        </p>
-                    </div>
                     <div class="d-flex align-items-center flex-nowrap">
                         <div class="bg-secondary rounded">
-                            <img src="Static/Images/testimonial-0.jpg" class="img-fluid rounded"
+                            <img src="Static/Images/mohamedAli.jpg" class="img-fluid rounded"
                                 style="width: 100px; height: 100px;" alt="">
                         </div>
                         <div class="ms-4 d-block">
-                            <h4 class="text-dark">Margaret</h4>
+                            <h4 class="text-dark">Mohamed Ali Guerfali</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="testimonial-item img-border-radius bg-light rounded p-4">
+                <div class="position-relative">
+                    <i class="fa fa-quote-right fa-2x text-secondary position-absolute"
+                        style="bottom: 30px; right: 0;"></i>
+                    <div class="d-flex align-items-center flex-nowrap">
+                        <div class="bg-secondary rounded">
+                            <img src="Static/Images/seif.jpg" class="img-fluid rounded"
+                                style="width: 100px; height: 100px;" alt="">
+                        </div>
+                        <div class="ms-4 d-block">
+                            <h4 class="text-dark">Seif Edin Chouchane</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="testimonial-item img-border-radius bg-light rounded p-4">
+                <div class="position-relative">
+                    <i class="fa fa-quote-right fa-2x text-secondary position-absolute"
+                        style="bottom: 30px; right: 0;"></i>
+                    <div class="d-flex align-items-center flex-nowrap">
+                        <div class="bg-secondary rounded">
+                            <img src="Static/Images/youssef.jpg" class="img-fluid rounded"
+                                style="width: 100px; height: 100px;" alt="">
+                        </div>
+                        <div class="ms-4 d-block">
+                            <h4 class="text-dark">Youssef Fadhloun</h4>
                         </div>
                     </div>
                 </div>
